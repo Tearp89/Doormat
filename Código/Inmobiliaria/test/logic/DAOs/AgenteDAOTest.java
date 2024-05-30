2a2a50c58a7eaac90f6443be88bcad26da0d5f10
Marla <121531470+Tearp89@users.noreply.github.com>
2024-05-30 17:56:24 -0600

    test

--- Código/Inmobiliaria/test/logic/DAOs/AgenteDAOTest.java
+++ Código/Inmobiliaria/test/logic/DAOs/AgenteDAOTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.sql.SQLException;
 
+import org.apache.log4j.jmx.Agent;
 import org.junit.Test;
 
 import logic.classes.Agente;
@@ -12,10 +13,34 @@ public class AgenteDAOTest {
     @Test
     public void testAgregarAgenteSuccess() throws SQLException {
         Agente agente = new Agente();
-        agente.setUsuarioAgente("Agente de Prueba");
+        agente.setUsuarioAgente("Agente2");
         agente.setContrasenia("testAgregarAgente");
         AgenteDAO agenteDAO = new AgenteDAO();
         int result = agenteDAO.agregarAgente(agente);
         assertEquals(1, result); 
     }
+
+
+    @Test
+    public void testAgendarVisita() throws SQLException{
+        Agente agente = new Agente();
+        agente.setUsuarioAgente("Agente prueba");
+        int resultadoEsperado = 0;
+        AgenteDAO agenteDAO = new AgenteDAO();
+        int resultado = agenteDAO.eliminarAgentePorUsuario(agente);
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+
+    @Test
+    public void testCalificarAgente() throws SQLException{
+        Agente agente = new Agente();
+        agente.setValoracion(5);
+        AgenteDAO gAgenteDAO = new AgenteDAO();
+        int resultadoEsperado = 1;
+        int resultado = gAgenteDAO.calificarAgente(agente);
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    
 }
--- Código/Inmobiliaria/test/logic/DAOs/PropiedadDAOTest.java
+++ Código/Inmobiliaria/test/logic/DAOs/PropiedadDAOTest.java
@@ -3,7 +3,9 @@ package logic.DAOs;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -29,6 +31,7 @@ public class PropiedadDAOTest {
         propiedad.setCochera(1);
         propiedad.setTamanio(150);
         propiedad.setResumen("Resumen de prueba");
+        propiedad.setUsuarioCliente(null);
 
         try {
             int result = propiedadDAO.agregarPropiedad(propiedad);
@@ -37,4 +40,82 @@ public class PropiedadDAOTest {
             fail("Excepción al agregar propiedad: " + e.getMessage());
         }
     }
+
+
+    @Test
+    public void TesteliminarPropiedadPorIdSuccess(){
+        PropiedadDAO propiedadDAO = new PropiedadDAO();
+        int resultadoEsperado = 1;
+        int idPropiedad = 3;
+        Propiedad propiedad = new Propiedad();
+        propiedad.setIdPropiedad(idPropiedad);
+        try{
+            int resultado = propiedadDAO.eliminarPropiedadPorId(propiedad);
+            assertEquals(resultadoEsperado, resultado);
+        } catch (SQLException e){
+            fail("Excepción al eliminar propiedad: " + e.getMessage());
+            
+        }
+
+    }
+
+    @Test
+    public void TestActualizarPropiedadPoridPropiedadSuccess(){
+        Propiedad propiedad = new Propiedad();
+        PropiedadDAO propiedadDAO = new PropiedadDAO();
+        propiedad.setDireccion("Lomas verdes #23");
+        propiedad.setDescripcion("Casa de 2 pisos acabado moderno.");
+        propiedad.setEstadoPropiedad("Disponible");
+        propiedad.setUsuarioAgente("Agente de Prueba");
+        propiedad.setCiudad("Xalapa");
+        propiedad.setZona("Zona trancas");
+        propiedad.setTipoPropiedad("Casa");
+        propiedad.setPrecio(1000);
+        propiedad.setNoHabitaciones(3);
+        propiedad.setNoEstancias(2);
+        propiedad.setNoBanios(2);
+        propiedad.setCochera(1);
+        propiedad.setTamanio(150);
+        propiedad.setResumen("Linda casa con mucho espacio");
+        propiedad.setIdPropiedad(4);
+        propiedad.setUsuarioCliente("cliente166");
+
+        try {
+            int result = propiedadDAO.actualizarPropiedadPorIdPropiedad(propiedad);
+            assertEquals(1, result); 
+        } catch (SQLException e) {
+            fail("Excepción al agregar propiedad: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void TestBuscarPropiedadesDeUsuarioPorUsuarioClienteSuccess(){
+        String nombreUsuario = "cliente166";
+        try{
+            PropiedadDAO propiedadDAO = new PropiedadDAO();
+            int resultadoEsperado = 0;
+            int resultado = propiedadDAO.buscarPropiedadesDeUsuarioPorUsuarioCliente(nombreUsuario).size();
+            assertEquals(resultadoEsperado, resultado);
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+       
+    }
+
+    @Test
+    public void TestCalificarPropiedad(){
+        int idPropiedad = 4;
+        int calificacion = 5;
+        PropiedadDAO propiedadDAO = new PropiedadDAO();
+        try{
+            int resultadoEsperado = 1;
+            int resultado = propiedadDAO.calificarPropiedad(calificacion, idPropiedad);
+            assertEquals(resultadoEsperado, resultado);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+   
 }
